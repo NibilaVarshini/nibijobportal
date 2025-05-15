f12cea4ae922e85416d5f09d98ba7617b95c0b73
Immanuvel M <mimmanuvel@genixtec.com>
2025-05-15 12:20:16 +0530

    new changes

--- routes/jobs.js
+++ routes/jobs.js
@@ -14,11 +14,11 @@ router.get('/getJobs', async (req, res) => {
 
 // POST /users
 router.post('/addJob', async (req, res) => {
-  const { title, position, location,jobreferenceid } = req.body;
+  const { title, position, location,jobreferenceid,companyname,description,salary} = req.body;
   try {
     const result = await db.query(
-      'INSERT INTO jobs (title, position, location,jobreferenceid) VALUES ($1, $2, $3,$4) RETURNING *',
-      [title, position, location,jobreferenceid]
+      'INSERT INTO jobs (title, position, location,jobreferenceid,companyname,description,salary) VALUES ($1, $2, $3,$4,$5,$6,$7) RETURNING *',
+      [title, position, location,jobreferenceid,companyname,description,salary]
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
